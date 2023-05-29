3c2234ed74819fb20009512ddbf30e035e713a3c
aj <art@ampsire.com>
2023-05-29 11:42:54 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'min-repo-latest-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'min-repo-latest-nx';
+}
